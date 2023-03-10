6a668c54575d2b69e09cae47852ac0ae49407bc2
mmesutkayaa <85591207+mmesutkayaa@users.noreply.github.com>
2023-03-10 03:36:22 +0300

    Created Upcoming Tab, Model
    Created Upcoming TableView inside Upcoming Tab, custom TableViewCell from the Upcoming Tab

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Mesut Kaya on 9.03.2023.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
