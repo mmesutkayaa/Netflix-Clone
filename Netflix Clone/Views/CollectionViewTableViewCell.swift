09a314a1e4c169abdfd5ae96b4324c6e9e0924a9
mmesutkayaa <85591207+mmesutkayaa@users.noreply.github.com>
2023-03-17 03:19:27 +0300

    Core Data, Notification center
    Added Core Data, Notification center

--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -55,7 +55,15 @@ class CollectionViewTableViewCell: UITableViewCell {
     }
     
     private func downloadTitleAt(indexPath: IndexPath) {
-        print("Downloading \(titles[indexPath.row].original_title)")
+        
+        DataPersistenceManger.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+                NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 }
